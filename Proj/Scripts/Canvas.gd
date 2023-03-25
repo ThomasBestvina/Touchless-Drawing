a1e364d0f094576e1a87da1a02ec8c2646c5e14f
Thomas <bestvinathomas@gmail.com>
2023-03-25 00:41:27 -0600

    rah

--- Proj/Scripts/Canvas.gd
+++ Proj/Scripts/Canvas.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 onready var tracker = $Tracker
-var brushtest = preload("res://TempBrush.tscn")
+var brushtest = preload("res://Scenes/TempBrush.tscn")
 
 var resolution = OS.get_screen_size()
 
