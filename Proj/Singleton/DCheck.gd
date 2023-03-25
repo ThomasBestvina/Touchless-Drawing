2f41805a27f011e06468f1e7ade98c501e000a2a
alexh2022 <98855779+alexh2022@users.noreply.github.com>
2023-03-24 22:42:24 -0600

    RQDFDFDFHMNGM V GN

--- Proj/Singleton/DCheck.gd
+++ Proj/Singleton/DCheck.gd
@@ -2,5 +2,8 @@ extends Node
 
 #Access with DCheck.distance()
 
-func distance():
-	pass
+func distance(x_2, x_1, y_2, y_1, z_2, z_1):
+	var dx = x_2 - x_1
+	var dy = y_2 - y_1
+	var dz = z_2 - z_1
+	return sqrt(dx * dx + dy * dy + dz * dz)
