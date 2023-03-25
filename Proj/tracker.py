36d110aa79711fe5801f816c2afe746ab40e5a61
Thomas <bestvinathomas@gmail.com>
2023-03-24 18:17:23 -0600

    rah

--- Proj/tracker.py
+++ Proj/tracker.py
@@ -13,6 +13,7 @@ class tracker(Node):
 	
 
 	def _ready(self):
+		#def to_arr_dict()
 		def tracker():
 			mp_drawing = mp.solutions.drawing_utils
 			mp_drawing_styles = mp.solutions.drawing_styles
@@ -39,6 +40,7 @@ class tracker(Node):
 					# Draw the hand annotations on the image.
 					image.flags.writeable = True
 					image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+					print(results.multi_hand_landmarks)
 					if results.multi_hand_landmarks:
 						for hand_landmarks in results.multi_hand_landmarks:
 							mp_drawing.draw_landmarks(
@@ -47,6 +49,7 @@ class tracker(Node):
 								mp_hands.HAND_CONNECTIONS,
 								mp_drawing_styles.get_default_hand_landmarks_style(),
 								mp_drawing_styles.get_default_hand_connections_style())
+							
 					# Flip the image horizontally for a selfie-view display.
 					cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
 					if cv2.waitKey(5) & 0xFF == 27:
